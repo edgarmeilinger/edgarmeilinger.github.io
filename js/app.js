65e680d9e37983cae4fdd38f406b79aa89b402b5
Edgar <edgar.meilinger@stud.fra-uas.de>
2022-01-19 18:33:05 +0100

    Revert "nur php test"
    This reverts commit 2851f33be98cfe642bb7e5cc00662c58ef00ec82.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+if (navigator.serviceWorker) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(regEvent => console.log("Service worker registered!"))
+      .catch(err => console.log("Service worker not registered"));
+  });
+}
